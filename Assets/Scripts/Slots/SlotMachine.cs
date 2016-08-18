f6bfc340e24dea9c014cc7e7b2d7b38353b577c0
Andrew Abriam <a_abriam@outlook.com>
2016-08-18 01:07:19 -0700

    Changed slot machine

--- Assets/Scripts/Slots/SlotMachine.cs
+++ Assets/Scripts/Slots/SlotMachine.cs
@@ -5,11 +5,11 @@ public class SlotMachine : MonoBehaviour {
 
     public GameObject s1, s2, s3;
 
-    MeshRenderer slot1, slot2, slot3;
+    private MeshRenderer slot1, slot2, slot3;
 
-    Material m1, m2, m3;
+    private Material m1, m2, m3;
 
-    Vector2 off1, off2, off3;
+    private Vector2 off1, off2, off3;
 
     bool occupied;
 
@@ -17,11 +17,7 @@ public class SlotMachine : MonoBehaviour {
 
     public bool roll; //engage spin
 
-   
     private float[] outcomes = new float[] { 0, 0.25f, 0.50f, 0.75f };
-
-    [SerializeField]
-    private int stopVal;
     
 
 	// Use this for initialization
@@ -41,8 +37,6 @@ public class SlotMachine : MonoBehaviour {
         spin1 = spin2 = spin3 = false;
         occupied = false;
         roll = false;
-
-        stopVal = 0;
 	
 	}
 	
@@ -56,7 +50,7 @@ public class SlotMachine : MonoBehaviour {
 
         if (spin1) //Spin slot reel 1 offset
         {
-            off1.y += 0.025f;
+            off1.y += 0.035f;
 
             if (off1.y > 1f)
             {
@@ -64,9 +58,10 @@ public class SlotMachine : MonoBehaviour {
             }
             
         }
+      
         if (spin2) //Spin slot reel 2 offset
         {
-            off2.y += 0.025f;
+            off2.y += 0.035f;
 
             if (off2.y > 1f)
             {
@@ -76,7 +71,7 @@ public class SlotMachine : MonoBehaviour {
         }
         if (spin3) //Spin slot reel 3 offset
         {
-            off3.y += 0.025f;
+            off3.y += 0.035f;
 
             if (off3.y > 1f)
             {
@@ -85,13 +80,12 @@ public class SlotMachine : MonoBehaviour {
 
         }
 
+        
+
         if (!occupied && roll)
         {
             StartCoroutine(stopSpin());
         }
-
-
-
 
 
         m1.mainTextureOffset = off1;
@@ -106,6 +100,32 @@ public class SlotMachine : MonoBehaviour {
 
         roll = true;
 
+        //Predetermine Outcomes//
+        int out1 = Random.Range(0, 4);
+        int out2 = 0; //Chance slot
+        int out3 = Random.Range(0, 4);
+
+        int chance = Random.Range(1, 3); //Either 1,2
+        if (chance == 1)  //Match either the 1st or 2nd reel 50% chance
+        {
+            int slot = Random.Range(1, 3);
+
+            if (slot == 1)
+            {
+                out2 = out1;
+            }
+            else
+            {
+                out2 = out3;
+            }
+
+        }
+        else
+        {
+            out2 = Random.Range(0, 4);
+        }
+        ////////////////////////
+
         yield return new WaitForSeconds(0f); //Start spinning slot 1;
         spin1 = true;
 
@@ -117,31 +137,30 @@ public class SlotMachine : MonoBehaviour {
 
         yield return new WaitForSeconds(3f); //Stop spinning slot 1
         spin1 = false;
-        int out1 = Random.Range(0, 4);
         off1.y = outcomes[out1]; //Assign random value outcome
 
         yield return new WaitForSeconds(0.5f); //Stop spinning slot 2
         spin2 = false;
-        int out2 = Random.Range(0, 4);
         off2.y = outcomes[out2]; //Assign random value outcome
+
+
+        
 
         yield return new WaitForSeconds(0.5f); //Stop spinning slot 3
         spin3 = false;
-        int out3 = Random.Range(0, 4);
         off3.y = outcomes[out3]; //Assign random value outcome
 
 
-
-
-
-
-
-
-
-
-
-
         roll = false;
+
+        if (off1 == off2 && off2 == off3)
+        {
+            print("WINNER!!!!");
+        }
+        else
+        {
+            print("LOSER :(");
+        }
 
         occupied = false;
 
